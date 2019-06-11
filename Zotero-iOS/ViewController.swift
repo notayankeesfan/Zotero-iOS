f55604a7f2d7f9048101030252eb006300fd1426
rohan-kadambi <rohan.kadambi@gatech.edu>
2019-06-10 20:24:18 -0700

    Initial Commit from Xcode with blank "Single View App"

---
+++ Zotero-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zotero-iOS
+//
+//  Created by Rohan Kadambi on 6/10/19.
+//  Copyright © 2019 Rohan Kadambi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
