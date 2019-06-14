b6fa7f5e80452da1b531f1f5a6d5f19e0a5277a9
rohan-kadambi <rohan.kadambi@gatech.edu>
2019-06-13 20:34:24 -0700

    Replaced Test Labels with rudimentary "Detailed Reference View (Ref Detail)" Currently missing scroll of fields. Added "edit" button for trigger testing, likely will not be included in rev 1.0. Renamed Base ViewController

--- Zotero-iOS/ViewController.swift
+++ Zotero-iOS/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Params
+    @IBOutlet weak var DocumentName: UITextField!
+    @IBOutlet weak var DocumentUUID: UITextField!
+    @IBOutlet weak var DocumentYear: UITextField!
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        DocumentName.text = "Test Name"
     }
 
-
+    //MARK: IBAction
+    @IBAction func ButtonPressEdit(_ sender: Any) {
+        print("Button Test")
+    }
 }
 
