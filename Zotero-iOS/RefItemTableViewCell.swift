529f0ecbc081685191c140445c6b649fa7f56696
rohan-kadambi <rohan.kadambi@gmail.com>
2019-06-13 22:45:58 -0700

    Created Ref Item Table Cell Class

---
+++ Zotero-iOS/RefItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RefItemTableViewCell.swift
+//  Zotero-iOS
+//
+//  Created by Rohan Kadambi on 6/13/19.
+//  Copyright © 2019 Rohan Kadambi. All rights reserved.
+//
+
+import UIKit
+
+class RefItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
