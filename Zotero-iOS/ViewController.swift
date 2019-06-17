be7d9df0880b78354fd0c3a6d95ef902316d5969
rohan-kadambi <rohan.kadambi@gmail.com>
2019-06-16 17:51:04 -0700

    Implemented Table view on RefDetail Scene which allows for expanding and hiding certain sections of the detail.
    Data Structure is temporary, will be moved to SQlMaster file
    
    Will Update by removing old data and setting up feed of actual data from SQLMaster

--- Zotero-iOS/ViewController.swift
+++ Zotero-iOS/ViewController.swift
@@ -8,19 +8,33 @@
 
 import UIKit
 
-class RefDetailController: UIViewController {
+class RefDetailController: UIViewController,
+        UITableViewDelegate, UITableViewDataSource{
 
     //MARK: Params
     @IBOutlet weak var DocumentName: UITextField!
     @IBOutlet weak var DocumentUUID: UITextField!
     @IBOutlet weak var DocumentYear: UITextField!
+    @IBOutlet weak var ContentsTable: UITableView!
     
     var UUID : Int = -1
     var documentTitle : String = ""
+    var fake_data = [ DetailPropertyCellContents(FieldName: "Name", Value: "Document Title is Something"),
+                      DetailPropertyCellContents(FieldName: "Test 1", Value: "test 1 text"),
+                      DetailPropertyCellContents(FieldName: "Year", Value: "2019"),
+                      DetailPropertyCellContents(FieldName: "Abstractt", Value: "Document Title is SomethingDocument Title is                                                           SomethingDocument Title is SomethingDocument                                                           Title is SomethingDocument Title is Something")
+                    ]
     
     //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Connect to Table
+        ContentsTable.delegate = self
+        ContentsTable.dataSource = self
+        
+        ContentsTable.estimatedRowHeight = 60
+        ContentsTable.rowHeight = UITableView.automaticDimension
+        
         // Do any additional setup after loading the view.
         DocumentName.text = "\(documentTitle)"
         DocumentUUID.text = "\(UUID)"
@@ -32,6 +46,36 @@ class RefDetailController: UIViewController {
         print("Button Test")
         // Toggle if field is editable
         DocumentName.allowsEditingTextAttributes = !DocumentName.allowsEditingTextAttributes
+    }
+    
+    
+    // Mark: Public Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //TBD
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TBD
+        return fake_data.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "DetailPropertyTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? DetailPropertyTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of DetailPropertyTableViewCell.")
+        }
+        cell.set(contents: fake_data[indexPath.row])
+        return cell
+    }
+    
+    // Mark: Nav
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let content = fake_data[indexPath.row]
+        content.Expanded = !content.Expanded
+        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
 }
 
