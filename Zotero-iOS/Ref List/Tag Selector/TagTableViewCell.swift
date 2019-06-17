67b4b82a90c48c70017c225bb98460ed27bdc4ee
rohan-kadambi <rohan.kadambi@gmail.com>
2019-06-16 20:55:08 -0700

    Added Tag Cell and Controller

---
+++ Zotero-iOS/Ref List/Tag Selector/TagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagTableViewCell.swift
+//  Zotero-iOS
+//
+//  Created by Rohan Kadambi on 6/16/19.
+//  Copyright © 2019 Rohan Kadambi. All rights reserved.
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
