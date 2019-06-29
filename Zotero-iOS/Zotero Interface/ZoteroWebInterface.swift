b2d74b4d2dd4642d09a63e4d946eb9a7aeb33e51
rohan-kadambi <rohan.kadambi@gmail.com>
2019-06-28 20:14:10 -0700

    Added Zotero Web Interface

---
+++ Zotero-iOS/Zotero Interface/ZoteroWebInterface.swift
@@ -0,0 +1,13 @@
+//
+//  ZoteroWebInterface.swift
+//  Zotero-iOS
+//
+//  Created by Rohan Kadambi on 6/28/19.
+//  Copyright © 2019 Rohan Kadambi. All rights reserved.
+//
+
+import Foundation
+
+class ZoteroWebInterface{
+    
+}
